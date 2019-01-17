8542f212551535fb45abd5cf9f0b9f423483873c
Michael Zhang <39073372+MichaelZhang181@users.noreply.github.com>
2019-01-16 23:50:14 -0500

    Removed BS4 and Pandas import
    Unnecessary imports.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import bs4
-import pandas as pd
-from bs4 import BeautifulSoup as soup
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+import csv
+
 
 #webpage for 2018-2019 season 
 stat_page = 'https://stats.nba.com/players/advanced/?sort=PLAYER_NAME&dir=-1'
@@ -16,8 +15,6 @@ combobox = nba_driver.find_element_by_xpath("/html/body/main/div/div/div/div/div
 table_all = Select(combobox)
 table_all.select_by_visible_text('All')
 
-# downloading the html file for parsing
-nba_html = nba_driver.page_source
 
 # closing the browser
 nba_driver.close()
